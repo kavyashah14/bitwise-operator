a364b26341225e2e6408ba3135e4ba2bc0c580c8
kavyashah14 <70435939+kavyashah14@users.noreply.github.com>
2023-03-14 09:47:41 +0530

    Add files via upload

---
+++ kavya XORoperator.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main()
+ {
+	int num1,num2,result;
+	num1=12;
+	num2=25;
+	result= num1 ^ num2;
+	
+	printf("num1 ^ num2 :%d\n", result);
+	return 0;
+}
+
+	
+
+
